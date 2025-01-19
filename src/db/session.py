390e2b57827212df038f701da40e29cc3d202d43
peri <76472728+wiuy3120@users.noreply.github.com>
2025-01-20 00:26:12 +0700

    feat: add get_db function

--- src/db/session.py
+++ src/db/session.py
@@ -3,10 +3,17 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-
 SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 print("Database URL is ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
