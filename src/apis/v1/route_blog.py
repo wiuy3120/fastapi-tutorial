6e5870f615f985e6d61a309e24fbbd392c36e54f
peri <76472728+wiuy3120@users.noreply.github.com>
2025-02-08 23:06:29 +0700

    feat(api): add `get_blog` route

--- src/apis/v1/route_blog.py
+++ src/apis/v1/route_blog.py
@@ -1,16 +1,27 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from db.repository.blog import create_new_blog
+from db.repository.blog import create_new_blog, retrieve_blog
 from db.session import get_db
 from schemas.blog import BlogCreate, ShowBlog
 
 router = APIRouter()
 
 
+@router.get("/blog/{id}", response_model=ShowBlog)
+def get_blog(id: int, db: Session = Depends(get_db)):
+    blog = retrieve_blog(id=id, db=db)
+    if not blog:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} is not found",
+        )
+    return blog
+
+
 @router.post(
     "/blogs", response_model=ShowBlog, status_code=status.HTTP_201_CREATED
 )
 def create_blog(blog: BlogCreate, db: Session = Depends(get_db)):
-    blog = create_new_blog(blog=blog, db=db)
-    return blog
+    new_blog = create_new_blog(blog=blog, db=db)
+    return new_blog
--- src/db/repository/blog.py
+++ src/db/repository/blog.py
@@ -5,8 +5,13 @@ from schemas.blog import BlogCreate
 
 
 def create_new_blog(blog: BlogCreate, db: Session, author_id: int = 1):
-    blog = Blog(**blog.model_dump(), author_id=author_id, is_active=True)
-    db.add(blog)
+    new_blog = Blog(**blog.model_dump(), author_id=author_id, is_active=True)
+    db.add(instance=new_blog)
     db.commit()
-    db.refresh(blog)
+    db.refresh(instance=new_blog)
+    return new_blog
+
+
+def retrieve_blog(id: int, db: Session):
+    blog = db.query(Blog).filter(Blog.id == id).first()
     return blog
